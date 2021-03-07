6a40bb3eb67317605a0e7fbc7bc58ed7e53362c7
akash-foodpanda <57343803+akash-foodpanda@users.noreply.github.com>
2021-03-07 20:12:23 +0800

    add a new post route to return string

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,9 +2,9 @@ import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
-    // uncomment to serve files from /Public folder
-    // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    // register routes
-    try routes(app)
+  // uncomment to serve files from /Public folder
+  // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+  
+  // register routes
+  try routes(app)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,11 +1,20 @@
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
-    }
+  app.get { req in
+    return "It works!"
+  }
+  
+  app.get("hello") { req -> String in
+    return "Hello, world!"
+  }
+  
+  app.post("info") { req -> String in
+    let data = try req.content.decode(InfoData.self)
+    return "Hello \(data.name)"
+  }
+}
 
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
+struct InfoData: Content {
+  let name: String
 }
